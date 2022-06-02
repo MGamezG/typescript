fb2d38f92011c4297cf063c9c89e2b9a18a36eda
MGamezG <l171130314@gamadero.tecnm.mx>
2022-06-02 11:51:48 -0500

    dia1

---
+++ typescript/app.js
@@ -0,0 +1,14 @@
+
+
+function saludar( nombre ) {
+    console.table( 'Hola ' + nombre ); // Hola Logan
+}
+
+
+const wolverine = {
+    nombre: 'Logan' // constante es un objeto literal
+};
+
+
+saludar(  );
+
